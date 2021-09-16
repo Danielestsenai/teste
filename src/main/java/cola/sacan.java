362dfdace912f86f9161c653084e20cb2fc5b503
Sistema Desenvolvimento <Daniel.Souza>
2021-09-16 20:22:59 -0300

    projeto inicial

---
+++ src/main/java/cola/sacan.java
@@ -0,0 +1,47 @@
+
+package cola;
+
+
+
+public class sacan {
+    
+public static void main(String[] args){
+    
+    
+    
+  System.out.println("informe seu nome:");   
+  String nome = scanner.nextline();
+
+  System.out.println ("imforme sua idade:");
+  int idade = scanner.nextlint();
+    
+
+  System.out.println ("imforme sua peso:");
+  float peso = scanner.nextlint();
+    
+
+  System.out.println ("voce cresceu :");
+  boolean  cresce();
+    
+
+  System.out.println ("informe sua altura:");
+  double altura = scanner.nextdouble();
+    
+System.out.println("******** seus dados cadastrado********");
+System.out.println("ola" + nome);
+ System.out.println("vi seu peso" + peso );
+  System.out.println("voce e velho em sua idade" + idade );
+  System.out.println("voce cresceu" + cresceu);
+  System.out.println("sua altura " + altura );
+   
+
+
+
+
+
+
+
+
+}
+
+}
